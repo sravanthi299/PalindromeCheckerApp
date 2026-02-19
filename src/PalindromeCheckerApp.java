2623125bf65f794c9210f6224a8c518b8c68c8e1
sravanthi299 <sravanthipragada545@gmail.com>
2026-02-19 10:01:42 +0530

    Initial commit

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,24 @@
+public class PalindromeCheckerApp {
+    /*
+    uc1MAIN CLASS UseCaseiPalindromeApp
+Use Case 1: Application Entry & Welcome Message
+* Description:
+This class represents the entry point of the
+Palindrome Checker Management System.
+At this stage, the application:
+Starts execution from the main() method
+Displays a welcome message
+Shows application version
+No palindrome Logic is implemented yet.
+The goal is to establish a clear startup flow.
+Bauthor Developer
+@version 1.0
+*/
+    public static void main(String[] args){
+        System.out.println("welcome to palindrome checker management system");
+        System.out.println("version:1.0");
+        System.out.println("system initialized  successfully");
+
+
+    }
+}
