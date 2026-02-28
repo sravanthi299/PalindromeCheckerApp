48b5c2d1b84555029c7f0468b5d492e5d7ba648a
sravanthi299 <sravanthipragada545@gmail.com>
2026-02-28 12:14:15 +0530

    UC5 Check palindrome using stack data structure

---
+++ src/UseCase5PalindromeCheckerApp.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class UseCase5PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input : ");
+        String input = sc.nextLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        // push characters into stack
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        boolean isPalindrome = true;
+
+        // compare with popped characters
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
