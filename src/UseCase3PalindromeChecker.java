68122c5ad7a823febcd2fad1dca3298b2e7a8868
sravanthi299 <sravanthipragada545@gmail.com>
2026-02-28 11:33:51 +0530

    UC3 Reverse String using loop and check palindrome

---
+++ src/UseCase3PalindromeChecker.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input text: ");
+        String input = sc.nextLine();
+
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+    }
+}
