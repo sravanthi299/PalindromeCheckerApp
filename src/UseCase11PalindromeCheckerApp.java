e849da5cad0041c7d12e6af69203a67d453597b6
sravanthi299 <sravanthipragada545@gmail.com>
2026-03-04 12:46:52 +0530

    Refactor palindrome checker into a class structure

--- src/UseCase11PalindromeCheckerApp.java
+++ src/UseCase11PalindromeCheckerApp.java
@@ -1,17 +1,24 @@
-public boolean checkPalindrome(String input) {
+public class UseCase11PalindromeCheckerApp {
+    public boolean checkPalindrome(String input) {
 
-    // Initialize pointers
-    int start = 0;
-    int end = input.length() - 1;
+        // Initialize pointers
+        int start = 0;
+        int end = input.length() - 1;
 
-    // Compare characters moving inward
-    while (start < end) {
-        if (input.charAt(start) != input.charAt(end)) {
-            return false;
+        // Compare characters moving inward
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
         }
-        start++;
-        end--;
+
+        return true;
     }
 
-    return true;
+    public static void main(String[] args) {
+        UseCase11PalindromeCheckerApp checker = new UseCase11PalindromeCheckerApp();
+        System.out.println(checker.checkPalindrome("racecar"));
+    }
 }
