888077077ac124e20225638d0debe0bce5adfbd5
sravanthi299 <sravanthipragada545@gmail.com>
2026-03-04 12:45:27 +0530

    Update UseCase12PalindromeCheckerApp.java

--- src/UseCase12PalindromeCheckerApp.java
+++ src/UseCase12PalindromeCheckerApp.java
@@ -1,19 +1,26 @@
-public boolean check(String input) {
+public class UseCase12PalindromeCheckerApp {
+    
+    public boolean check(String input) {
+        // Create a stack to store characters
+        java.util.Stack<Character> stack = new java.util.Stack<>();
 
-    // Create a stack to store characters
-    java.util.Stack<Character> stack = new java.util.Stack<>();
-
-    // Push each character of the input string onto the stack
-    for (char c : input.toCharArray()) {
-        stack.push(c);
-    }
-
-    // Compare characters by popping from the stack
-    for (char c : input.toCharArray()) {
-        if (c != stack.pop()) {
-            return false;
+        // Push each character of the input string onto the stack
+        for (char c : input.toCharArray()) {
+            stack.push(c);
         }
-    }
 
-    return true;
+        // Compare characters by popping from the stack
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        UseCase12PalindromeCheckerApp app = new UseCase12PalindromeCheckerApp();
+        System.out.println(app.check("racecar"));
+    }
 }
