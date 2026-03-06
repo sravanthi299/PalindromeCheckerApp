0e46ebd3d1a235009657d6252eadfa796d178b1f
sravanthi299 <sravanthipragada545@gmail.com>
2026-03-06 10:22:44 +0530

    Update UseCase12PalindromeCheckerApp.java

--- src/UseCase12PalindromeCheckerApp.java
+++ src/UseCase12PalindromeCheckerApp.java
@@ -1,19 +1,65 @@
-public boolean check(String input) {
+/**
+ * Use Case 11: Object-Oriented Palindrome Service
+ *
+ * Description:
+ * This class demonstrates palindrome validation using
+ * object-oriented design.
+ *
+ * The palindrome logic is encapsulated inside a
+ * PalindromeService class.
+ *
+ * This improves:
+ * - Reusability
+ * - Maintainability
+ * - Separation of concerns
+ *
+ * @author Developer
+ * @version 1.0
+ */
 
-    // Create a stack to store characters
-    java.util.Stack<Character> stack = new java.util.Stack<>();
+public class UseCase11PalindromeCheckerApp {
 
-    // Push each character of the input string onto the stack
-    for (char c : input.toCharArray()) {
-        stack.push(c);
+    /**
+     * Application entry point
+     */
+    public static void main(String[] args) {
+
+        String input = "level";
+
+        PalindromeService service = new PalindromeService();
+
+        boolean result = service.checkPalindrome(input);
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome : " + result);
     }
+}
 
-    // Compare characters by popping from the stack
-    for (char c : input.toCharArray()) {
-        if (c != stack.pop()) {
-            return false;
+/**
+ * Service class that contains palindrome logic
+ */
+class PalindromeService {
+
+    /**
+     * Checks whether the input string is a palindrome
+     * @param input Input string
+     * @return true if palindrome, false otherwise
+     */
+    public boolean checkPalindrome(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
         }
-    }
 
-    return true;
+        return true;
+    }
 }
