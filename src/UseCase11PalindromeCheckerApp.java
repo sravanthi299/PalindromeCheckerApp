8d4ff1a5f22afe45d48d1708538856b660a1be7b
sravanthi299 <sravanthipragada545@gmail.com>
2026-03-06 10:16:16 +0530

    Refactor palindrome logic into PalindromeService class
    Refactored palindrome checking logic into a separate PalindromeService class for better reusability and maintainability.

--- src/UseCase11PalindromeCheckerApp.java
+++ src/UseCase11PalindromeCheckerApp.java
@@ -1,17 +1,65 @@
-public boolean checkPalindrome(String input) {
+/**
+ * Use Case 11: Object-Oriented Palindrome Service
+ *
+ * Description:
+ * This class demonstrates palindrome validation using
+ * object-oriented design.
+ *
+ * The palindrome logic is encapsulated inside a
+ * PalindromeService class.
+ *
+ * This improves:
+ * - Reusability
+ * - Maintainability
+ * - Separation of concerns
+ *
+ * @author Developer
+ * @version 1.0
+ */
 
-    // Initialize pointers
-    int start = 0;
-    int end = input.length() - 1;
+public class UseCase11PalindromeCheckerApp {
 
-    // Compare characters moving inward
-    while (start < end) {
-        if (input.charAt(start) != input.charAt(end)) {
-            return false;
-        }
-        start++;
-        end--;
+    /**
+     * Application entry point
+     */
+    public static void main(String[] args) {
+
+        String input = "level";
+
+        PalindromeService service = new PalindromeService();
+
+        boolean result = service.checkPalindrome(input);
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome : " + result);
     }
+}
 
-    return true;
+/**
+ * Service class that contains palindrome logic
+ */
+class PalindromeService {
+
+    /**
+     * Checks whether the input string is a palindrome
+     * @param input Input string
+     * @return true if palindrome, false otherwise
+     */
+    public boolean checkPalindrome(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
 }
